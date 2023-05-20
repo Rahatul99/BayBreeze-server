69799c98f8e1bcbe8a769bce08169ab29d73b0b0
rahatul <rahatul99@gmail.com>
2023-05-20 13:59:22 +0600

    toys data loaded from mongoDb

--- index.js
+++ index.js
@@ -8,6 +8,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+//mongo
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jp6ok1r.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    const toysCollection = client.db('toysDB').collection('categories');
+
+    app.get('/categories', async(req, res) => {
+      const result = await toysCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//mongo
+
 app.get('/', (req, res) => {
     res.send('toy market place')
 })
